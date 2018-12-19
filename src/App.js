f35eac9974f3437c09e93c5b22c219cee09d61c3
rowan <rowan@rowan.website>
2018-12-19 02:42:34 -0500

    Beginning of proof-of-concept for hooking up the react frontend to python backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.updateDMX = this.updateDMX.bind(this)
+  }
+
+  updateDMX () {
+    console.log('updateDMX')
+    let payload =
+    {
+      "channels_list": [
+        {"channel": 50, "value": 255},
+        {"channel": 51, "value": 0},
+        {"channel": 52, "value": 0}
+      ]
+    }
+    console.log(payload)
+
+    fetch('http://127.0.0.1:8000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      }
+    ).then(response => console.log(response.status))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          type="button"
+          value="click me"
+          onClick={this.updateDMX} />
       </div>
     );
   }
