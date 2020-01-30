c52fc2a86c4930b5166060332fe2bc9bbcedafab
rowan <rowan@rowan.website>
2020-01-29 23:51:43 -0500

    General cleanup

--- src/component/LEDParCan.js
+++ src/component/LEDParCan.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ColorXY, DeviceLabel, Fader } from './Widget';
+import { ColorXY, DeviceLabel } from './Widget';
 import './LEDParCan.css'
 
 export default class LEDParCan extends Component {
@@ -16,22 +16,16 @@ export default class LEDParCan extends Component {
     }
   }
 
-  scaleBrightness = (color, brightness) => {
-    let percent = brightness / 100
-    color *= percent
-    return Math.floor(color)
-  }
   parseToDMX = (rgb) => {
     // take a rgb value and break it up into dmx messages
     let address0 = this.state.device.address
     let address1 = address0 + 1; let address2 = address1 + 1
-    let scaleBrightness = this.scaleBrightness
-    let brightness = this.state.fader.value
+
     return {
       "channels_list": [
-        { "channel": address0, "value": scaleBrightness(rgb.r, brightness) },
-        { "channel": address1, "value": scaleBrightness(rgb.g, brightness) },
-        { "channel": address2, "value": scaleBrightness(rgb.b, brightness) },
+        { "channel": address0, "value": rgb.r },
+        { "channel": address1, "value": rgb.g },
+        { "channel": address2, "value": rgb.b },
       ]
     }
 
@@ -61,13 +55,6 @@ export default class LEDParCan extends Component {
           name="color"
           storedValue={this.state.color.value}
           handleChange={this.handleColorChange}
-          />
-        <Fader
-          name="fader"
-          storedValue={this.state.fader.value}
-          style={this.state.fader.style}
-          propertyName={this.state.fader.property}
-          handleChange={this.handleChange}
           />
       </div>)
   }
