7b801b977aba482318443872ee016de52f5614bd
rowan <rowan@rowan.website>
2020-02-03 18:43:00 -0500

    Update server address

--- src/component/App.js
+++ src/component/App.js
@@ -55,7 +55,7 @@ export default class App extends Component {
   }
 
   updateDMX (payload) {
-    fetch('http://dmx.local', {
+    fetch('http://api.dmx.local', {
         method: 'post',
         headers: {
           'Content-Type': 'application/json'
